828530206965d62b2b38c55da566965a1971cc80
parham-devdocs <p.77.pazargadi@gmail.com>
2025-01-17 11:58:02 +0330

    header gradiant changed

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -5,7 +5,7 @@ const Header = ({ children }: { children: string }) => {
     return (
         <p
             
-            className="text-[40px] font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-800"
+            className="text-[40px] font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-green-800"
         >
             {children}
         </p>
