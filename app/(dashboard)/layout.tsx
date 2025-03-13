6b3dd408b88f9ae19d4d912c0ad96f56bcccd18f
parham <p1377parsa@gmail.com>
2025-03-13 22:14:42 +0330

    sidebar active link fixed

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from "react";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/admin/dashboard/sidebar";
+import { AppSidebar } from "@/components/common/sidebar";
 import Navbar from "@/components/common/navbar";
 import { cookies } from "next/headers";
 import { Link } from "@/types";
-const sideBarItems:Link[]=[
-  {title:"doctors",url:"/admin/dosctorsList",icon:"stethoscope"},
-  {title:"patients",url:"/admin/patientsList",icon:"user-round-cog"},
-  {title:"statistics",url:"/admin/statistics",icon:"chart-pie"}
-]
+const sideBarItems: Link[] = [
+  { title: "doctors", url: "/admin/doctors", icon: "stethoscope" },
+  { title: "patients", url: "/admin/patient", icon: "user-round-cog" },
+  { title: "statistics", url: "/admin/statistics", icon: "chart-pie" },
+];
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   const cookieStore = await cookies();
   const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
@@ -16,14 +16,11 @@ const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   return (
     <div className=" h-screen w-screen">
       <SidebarProvider defaultOpen={defaultOpen}>
-        <AppSidebar items={sideBarItems}  />
+        <AppSidebar items={sideBarItems} />
         <main className=" w-[97vw]  ">
-          <Navbar/>
-          <hr/>
-                  <div className="px-6">
-                             {children}
-   
-</div>
+          <Navbar />
+          <hr />
+          <div className="px-6">{children}</div>
         </main>
       </SidebarProvider>
     </div>
--- components/common/sidebar.tsx
+++ components/common/sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {  LogOut,Stethoscope,UserRoundCog,ChartPie } from "lucide-react";
+import { LogOut, Stethoscope, UserRoundCog, ChartPie } from "lucide-react";
 import { useSidebar } from "@/components/ui/sidebar";
 import {
   Sidebar,
@@ -10,19 +10,22 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { Button } from "../../ui/button";
+import { Button } from "../ui/button";
 import Image from "next/image";
 import { Link } from "@/types";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
-
-export function AppSidebar({items}:{items:Link[]}) {
-  const { setOpen ,open} = useSidebar();
+export function AppSidebar({ items }: { items: Link[] }) {
+  const pathname=usePathname()
+  
+  const { setOpen, open } = useSidebar();
   const iconMap: Record<string, React.ComponentType> = {
     "chart-pie": ChartPie,
     "user-round-cog": UserRoundCog,
-    "stethoscope": Stethoscope,
+    stethoscope: Stethoscope,
   };
-  
+
   return (
     <Sidebar
       className=" bg-background"
@@ -34,25 +37,28 @@ export function AppSidebar({items}:{items:Link[]}) {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              <SidebarMenuItem className={open?"mx-12":""}>
+              <SidebarMenuItem className={open ? "mx-12" : ""}>
                 <Image
                   src={"/unknownUser.jpg"} // Use empty string if no image
                   alt="profile image" // Use name as alt text
                   width="200"
                   height="200"
-                  className=" rounded-full border-2 border-primary"
+                  className=" rounded-full border-2 border-primary mb-2"
                 />
               </SidebarMenuItem>
               {items.map((item) => {
-const IconComponent=iconMap[item.icon]
-                return <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                    {IconComponent && <IconComponent />} {/* Render the icon */}                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-})}
+                const IconComponent = iconMap[item.icon];
+                return (
+                  <SidebarMenuItem key={item.title} className={ clsx( item.url===pathname ? "bg-primary rounded-lg":"" , "mt-1")}>
+                    <SidebarMenuButton asChild >
+                      <a href={item.url}>
+                        {IconComponent && <IconComponent />}{" "}
+                        {/* Render the icon */} <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
